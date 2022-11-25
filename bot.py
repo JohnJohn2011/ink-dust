71f6b32cde5cef483fe096312b9cc4aecbf8d8e5
JohnJohn2011 <119047873+JohnJohn2011@users.noreply.github.com>
2022-11-25 20:42:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix= '!')
 
 @bot.event
-async def funcname
+async def funcname(parameter_list):
+    print(">> bot is online <<")
+
+bot.run('MTA0NDIwNDUzMjQ1ODA2MTgyNQ.GGXI9p.lTagWO8Bnw3OZwWQCTnc7Hq-VzMWXMqNCQCKtE')
